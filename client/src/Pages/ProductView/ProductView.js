df05ec89cde971b51dec4760864b37846f9fe099
Iskandar Kurbanov <iskurbanov@gmail.com>
2019-05-23 16:53:01 -0400

    resolve conflict

--- client/src/Pages/ProductView/ProductView.js
+++ client/src/Pages/ProductView/ProductView.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import ProductItem from './ProductItem';
+import myJson from '../../data/fakeData.json';
 import backArrow from '../../assets/icons/SVG/Icon-back-arrow.svg';
 import './products.scss';
 
 
 export class ProductView extends Component {
     state = {
-
+        warehouseID: this.props
     }
 
     render() {
+        const 
         return (
             <div>
                 <div className="product-page__header">
                     <img src={backArrow} className="product-page__arrow" alt="back arrow" />
                     <h1 className="product-page__title">
-                        Product Name
+                        
                     </h1>
+
                 </div>
                 <ProductItem />
                 <button className="product-page__button">EDIT</button>
