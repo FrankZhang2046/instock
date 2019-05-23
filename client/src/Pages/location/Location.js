504b50549fc33d1f43093520b5ac6e8f077e4631
Joshua Taguicana <joshtaguicana@gmail.com>
2019-05-23 12:13:19 -0400

    locations page completed

--- client/src/Pages/location/Location.js
+++ client/src/Pages/location/Location.js
@@ -15,6 +15,14 @@ export class Location extends Component {
                     <h1 className="headerSearchBar__header" >Locations</h1>
                     <input className="headerSearchBar__searchBar" type="text"  placeholder="search"></input>
                 </div>
+                <div className="tableLabels">
+                    <p className="tableLabels__warehouse">WAREHOUSE</p>
+                    <div className="tableLabels__info">
+                        <p className="tableLabels__info__labels">CONTACT</p>
+                        <p className="tableLabels__info__labels">CONTACT INFORMATION</p>
+                        <p className="tableLabels__info__labels">CATEGORIES</p>
+                    </div>
+                </div>
                 {this.state.warehouseData.map((items, index) => 
                     <LocationDetails 
                         warehouseData={items}
