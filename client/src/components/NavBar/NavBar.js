4ae57a57cf72d55a3d6706263df2422f890823ea
Iskandar Kurbanov <iskurbanov@gmail.com>
2019-05-23 10:02:32 -0400

    fixed some bugs and removed old files

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo/Logo-instock.svg';
+import './NavBar.scss';
 
 export class NavBar extends Component {
     render() {
@@ -8,9 +9,9 @@ export class NavBar extends Component {
             <div>
                 <nav>
                     <div className="nav">
-                        <a href="./biography.html" className="nav__logo-link">
+                        <Link to="/">
                             <img src={logo} alt="BandSite logo" className="nav__logo" />
-                        </a>
+                        </Link>
                         <ul className="nav__links">
                         <Link to="/inventory">
                             <li className="nav__links-bio" id="nav__current">Inventory</li>
