1a4f8d0d9e9d739ec66740bea0abcc942947edde
Iskandar Kurbanov <iskurbanov@gmail.com>
2019-05-26 14:34:59 -0400

    warehouse

--- client/src/Pages/ProductView/ProductView.js
+++ client/src/Pages/ProductView/ProductView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import axios from 'axios';
+import axios from 'axios';
 // import shortid from 'shortid';
 import ProductItem from './ProductItem';
 import backArrow from '../../assets/icons/SVG/Icon-back-arrow.svg';
@@ -9,15 +9,27 @@ import './products.scss';
 
 export class ProductView extends Component {
     state = {
-        inventory: [],
-
         warehouse : []
+    }
 
+    componentDidMount(){
+        axios.get(`http://localhost:8080/warehouse`)
+            .then(result => {
+                this.setState({warehouse: result.data}
+                    )
+                console.log(this.state.warehouse)}
+            )
     }
 
     render() {
 
-        const { categories, description, lastOrdered, location, name, quantity, isInstock } = this.props.location.state.test
+        const { categories, description, lastOrdered, location, name, quantity, isInstock, warehouseId } = this.props.location.state.test;
+        const currentWarehouse = this.state.warehouse
+                                    .filter(warehouse => 
+                                    warehouse.id === warehouseId
+                                );
+        console.log(currentWarehouse);
+
         return (
             <div className="product-page">
                 <div className="product-page__header">
@@ -30,7 +42,7 @@ export class ProductView extends Component {
                 </div>
                 <ProductItem 
                     description={description} 
-                    name={name}
+                    // name={currentWarehouse.contact.name}
                     lastOrdered={lastOrdered}
                     location={location}
                     quantity={quantity}
