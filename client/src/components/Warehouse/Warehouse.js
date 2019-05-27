767f6607238eb2db4d008b7447219b3819f59e9e
nicoguname@gmail.com <nicoguname@gmail.com>
2019-05-27 14:35:09 -0400

    resolved filtering code to display warehouse info

--- client/src/components/Warehouse/Warehouse.js
+++ client/src/components/Warehouse/Warehouse.js
@@ -1,31 +1,35 @@
 
 import React, { Component } from 'react'
 // import warehousedata from './data1.js';
-// import inventorydata from './data2.js';
-// import InventoryData from './InventoryData.js';
+import inventorydata from './data2.js';
+import InventoryData from './InventoryData.js';
 import WarehouseData from './WarehouseData.js';
-import Inventory from '../../Pages/Inventory/Inventory.js';
+// import Inventory from '../../Pages/Inventory/Inventory.js';
 import warehouseData from '../../Pages/location/data.js';
 // import Location from '../LocationDetails/LocationDetails.js';
 
 export class Warehouse extends Component {
 
     state = {
-        warehouseData
+        warehouseData,
+        inventorydata
     }
 
   render() {
+console.log(warehouseData)
+console.log(warehouseData[1].id)
+console.log(inventorydata)
 
     return (
-      <div>
-        <WarehouseData warehouseData={warehouseData[0]}/>
-        {/* {this.state.warehouseData.map((items, index) => 
-          <WarehouseData
-              warehouseData={items}
-              key={index} />)        
-        } */}
-           <Inventory /> 
-               
+      
+      <div>   
+        <WarehouseData warehouseData={this.state.warehouseData.find(items => items.id === 'W0')}/>
+
+        {this.state.inventorydata.map((items, index) => 
+          <InventoryData
+              inventorydata={items}
+              key={index} />) 
+        }      
       </div>
      
     );
--- client/src/components/Warehouse/WarehouseData.js
+++ client/src/components/Warehouse/WarehouseData.js
@@ -1,45 +1,52 @@
-import React, { Component } from 'react'
-import iconback from '../../assets/icons/SVG/Icon-back-arrow.svg';
-import Inventory from '../../Pages/Inventory/Inventory.js';
-import './styles.scss'
+import React, { Component } from "react";
+import iconback from "../../assets/icons/SVG/Icon-back-arrow.svg";
+// import Inventory from '../../Pages/Inventory/Inventory.js';
+import "./styles.scss";
 
 export class WarehouseData extends Component {
-
-    render() {
-
+  render() {
     const { name, address, contact } = this.props.warehouseData;
 
-    console.log(this.props.warehouseData.id)
+    console.log(this.props.warehouseData);
 
-        return (
-            <div className="header">
-                <p><div className="header__title"> 
-                    <div className="header__title--icon">    
-                        <img src={iconback} alt="iconback"/> </div>
-                        <div className="header__title--name">{name} </div>      
-                </div>
-                </p>
-                <div className="header__address">ADDRESS</div>
-                <p><div className="header__street">{address.street}</div>
-                    <div className="header__number">{address.suiteNum}</div> 
-                </p>
-                <p><div className="header__city">{address.city}</div>
-                    <div className="header__postal">{this.props.warehouseData.address.postal}</div> 
-                </p>
-                <div className="header__contact">CONTACT</div>
-                <p> <div className="header__name">{contact.name}</div>
-                    <div className="header__position">{contact.title}</div> 
-                </p>
-                <p><div className="header__phone">{contact.phone}</div>
-                    <div className="header__email">{contact.email}</div> 
-                </p>
-                    
-                 {/* <Inventory />    */}
-                 
-                    
+    return (
+      <div className="header">
+        <p>
+          <div className="header__title">
+            <div className="header__title--icon">
+              <img src={iconback} alt="iconback" />
             </div>
-        )
-    }
+            <div className="header__title--name">{name} </div>
+          </div>
+        </p>
+        <div classname="header__all">
+          <div className="header__all__place1">
+            <div className="header__address">ADDRESS</div>
+            <p>
+              <div className="header__street">{address.street}</div>
+              <div className="header__number">{address.suiteNum}</div>
+            </p>
+            <p>
+              <div className="header__city">{address.city}</div>
+              <div className="header__postal">{address.postal}</div>
+            </p>
+          </div>
+          <div className="header__all__place2">
+            <div className="header__contact">CONTACT</div>
+            <p>
+              {" "}
+              <div className="header__name">{contact.name}</div>
+              <div className="header__position">{contact.title}</div>
+            </p>
+            <p>
+              <div className="header__phone">{contact.phone}</div>
+              <div className="header__email">{contact.email}</div>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default WarehouseData;
