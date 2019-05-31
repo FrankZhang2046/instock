52589f4b07bec83a2f707d463c47ce3011f56dfe
nicoguname@gmail.com <nicoguname@gmail.com>
2019-05-31 10:50:04 -0400

    added MissingPage component to display 404 page not found error message

--- client/src/components/MissingPage/MissingPage.js
+++ client/src/components/MissingPage/MissingPage.js
@@ -1,16 +1,15 @@
 import React from 'react';
-// import image from '../../assets/images/wesley-tingey-1500706-unsplash.jpg';
+import image from '../../assets/images/wesley-tingey-1511140-unsplash.jpg';
 import './styles.scss';
 
 class MissingPage extends React.Component {
     render () {
         return (
             <div className="missing">
-                {/* <img src="https://images.unsplash.com/photo-1499988921418-b7df40ff03f9" alt="nothing" className="missing__image"/> */}
-                {/* <img src={image} alt="nothing" className="missing__image"/> */}
+                <img src={image} alt="nothing" className="missing__image"/>
                 <div className="missing__content">
-                    <h1 className="missing__title">404</h1>
-                    <h2 className="missing__message">Awwwh!...The requested page is not found...</h2>
+                    <h1 className="missing__content--title">404</h1>
+                    <h2 className="missing__content--message">Awwwh!...The requested page is not found...</h2>
                 </div>
             </div>
         )
